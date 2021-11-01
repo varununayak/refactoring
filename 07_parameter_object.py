d802b8bdc8a8825ca53b7bec3d3ce99080969db6
varununayak <varunnayak3101@gmail.com>
2021-10-31 19:10:04 -0700

    feat: parameter object

---
+++ 07_parameter_object.py
@@ -0,0 +1,45 @@
+# Introduce Parameter Object
+
+from typing import List
+
+def readings_within_range(readings: List[float], min: float, max: float) -> bool:
+    return all([reading < max and reading > min for reading in readings])
+
+"""
+Data items that regularly travel together, appearing in function after function are known as a 
+data clump. Such data clumps are best replaced with a single data structure.
+
+The process can also improve the conceptual picture of the code, raising the data structures
+as new abstractions that can greatly simplify the understanding of the domain of the code.
+"""
+
+class Range:
+
+    def __init__(self, min: float, max : float) -> None:
+        self.min = min
+        self.max = max
+
+
+def readings_within_range_refactored(readings: List[float], range : Range) -> bool:
+    return all([reading < range.max and reading > range.min for reading in readings])
+
+"""
+Making the range a class makes it easier to move functionality into the class itself if required
+"""
+
+class RangeBetter:
+
+    def __init__(self, min: float, max : float) -> None:
+        self.min = min
+        self.max = max
+
+    def within_range(self, value: float) -> bool:
+        return value > self.min and value < self.max
+
+
+if __name__ == "__main__":
+    readings = [24, 12, 2, 10]
+    print(f"Within range: {readings_within_range(readings, 0, 20)}")
+    print(f"Within range: {readings_within_range_refactored(readings, Range(0, 20))}")
+    range = RangeBetter(0, 20)
+    print(f"Within range: {all([range.within_range(reading) for reading in readings])}")
