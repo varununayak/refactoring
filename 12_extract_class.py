22e1e9787c2d0a054550d85870d2b89f461ca523
varununayak <varunnayak3101@gmail.com>
2021-11-02 20:55:04 -0700

    feat: extract class

---
+++ 12_extract_class.py
@@ -0,0 +1,42 @@
+# Extract Class
+
+class Person:
+
+    def __init__(self, name: str, office_area_code : int, office_number: int) -> None:
+        self.name = name
+        self.office_area_code = office_area_code
+        self.office_number = office_number
+
+    @property
+    def telephone_number(self) -> str:
+        return f"({self.office_area_code})-{self.office_number}"
+
+"""
+Classes can sometimes get very big, affecting readability and complicated to add more features too.
+At such a time it makes sense to split the class into two (or more) classes..
+A good sign that the class can be split is when a subset of the data and a subset of the methods
+go together, or subsets of data change together or are particularly dependent on each other.
+Good abstractions help reveal these separations.
+"""
+
+class TelephoneNumber:
+
+    def __init__(self, office_area_code: int, office_number : int) -> None:
+        self.office_area_code = office_area_code
+        self.office_number = office_number
+
+    def __repr__(self) -> str:
+        return f"({self.office_area_code})-{self.office_number}"
+
+class Person2:
+
+    def __init__(self, name: str, office_area_code: int, office_number: int) -> None:
+        self.name = name
+        self.telephone_number = TelephoneNumber(office_area_code, office_number)
+
+
+if __name__ == "__main__":
+    p1 = Person("Varun", 650, 111111)
+    print(f"Phone number of {p1.name} is {p1.telephone_number}")
+    p2 = Person2("Varun", 650, 111111)
+    print(f"Phone number of {p2.name} is {p2.telephone_number}")
